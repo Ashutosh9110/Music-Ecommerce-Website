5b567a65d8f5fa696284b486d5ecf8735c62648b
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-09-29 11:57:32 +0530

    added Cart.jsx functionality in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-
-import './App.css'
-import Products from './components/Products';
+import React, { useState } from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import "./App.css"
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
   return (
     <div>
-      <h2 className="text-center mt-4">E-Commerce Store</h2>
+      {/* Navbar with Cart Icon */}
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">E-Commerce Store</Navbar.Brand>
+          <Nav className="ms-auto">
+            <Button variant="outline-light" onClick={() => setShowCart(true)}>
+              🛒 Cart
+            </Button>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      {/* Product List */}
+      <h2 className="text-center mt-4">Products</h2>
       <Products />
+
+      {/* Cart Modal */}
+      <Cart show={showCart} handleClose={() => setShowCart(false)} />
     </div>
   );
 }
-export default App
+
+export default App;
